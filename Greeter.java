aa674e5d61ad47ed76dd7777962300230db5350f
wendi030717 <3031764612@qq.com>
2023-09-08 21:55:50 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -19,7 +19,7 @@ public class Greeter {
   public static void main(String[] args) {
     String language = args[0];
     String country = args[1];
-    String name = args[2];
+    String name = args[5];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
